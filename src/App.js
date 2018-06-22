ee2ebea595cbd11329ae3b9b30885bc45750e720
Alexandr Loginov <atatakobry@gmail.com>
2018-06-22 22:36:35 +0300

    add support of css modules and scss (using react-app-rewired); just waiting for create-reat-app 2.0 :(

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+import styles from './App.module.scss';
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return <div className={styles.App} />;
   }
 }
 
