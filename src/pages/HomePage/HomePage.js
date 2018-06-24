140d16b19bf1e5682e8e9feba5e64e530836a4c0
Alexandr Loginov <atatakobry@gmail.com>
2018-06-24 23:17:36 +0300

    add cutie image to home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import styles from './HomePage.module.scss';
+
+import image from './irasshaimase.png';
+
 function HomePage() {
   return (
     <div>
-      <h1>いらっしゃいませ！</h1>
+      <img className={styles.Image} src={image} alt="irasshaimase" />
     </div>
   );
 }
