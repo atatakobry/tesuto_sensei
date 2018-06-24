321f480ca064d29c012f00d3fdc569b7889e6aeb
Alexandr Loginov <atatakobry@gmail.com>
2018-06-24 14:24:39 +0300

    remove unused imported component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 
 import styles from './App.module.scss';
 
