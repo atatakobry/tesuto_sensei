f3a938fc72373e627e7f791d612dcb89d10714c9
Alexandr Loginov <atatakobry@gmail.com>
2018-07-08 01:11:35 +0300

    a little bit refactoring of `exerciseColumns` in exercises list

--- src/pages/ExercisesPage/ExercisesList/exerciseColumns.js
+++ src/pages/ExercisesPage/ExercisesList/exerciseColumns.js
@@ -10,144 +10,83 @@ import {
 } from '../../../graphql/exercises';
 import { onExerciseDeleteConfirm } from '../../../common';
 
+const optionsColumn = {
+  title: (
+    <code>
+      <strong>Options</strong>
+    </code>
+  ),
+  dataIndex: 'options',
+  render: options => <pre>{JSON.stringify(options, null, 2)}</pre>
+};
+
+const answerColumn = {
+  title: (
+    <code>
+      <strong>Answer</strong>
+    </code>
+  ),
+  dataIndex: 'answer',
+  render: answer => <code>{answer}</code>
+};
+
+const actionsColumn = {
+  width: '110px',
+  render: ({ type, id }) => (
+    <Route
+      render={({ history }) => (
+        <div style={{ textAlign: 'center' }}>
+          <Mutation
+            mutation={DELETE_EXERCISE}
+            variables={{ id }}
+            update={(cache, { data: { deleteExercise } }) => {
+              const { exercises } = cache.readQuery({
+                query: GET_EXERCISES_BY_TYPE,
+                variables: { typeUid: type.uid }
+              });
+
+              cache.writeQuery({
+                query: GET_EXERCISES_BY_TYPE,
+                variables: { typeUid: type.uid },
+                data: {
+                  exercises: exercises.filter(
+                    ({ id }) => id !== deleteExercise.id
+                  )
+                }
+              });
+            }}
+          >
+            {(deleteExercise, { loading }) => (
+              <Button
+                size="small"
+                type="danger"
+                icon="delete"
+                loading={loading}
+                onClick={() =>
+                  onExerciseDeleteConfirm({ onOk: deleteExercise })
+                }
+              />
+            )}
+          </Mutation>
+
+          <Divider type="vertical" />
+
+          <Button
+            size="small"
+            icon="eye"
+            onClick={() => {
+              history.push(`/exercises/${id}`);
+            }}
+          />
+        </div>
+      )}
+    />
+  )
+};
+
 const exerciseColumns = {
-  [exerciseTypes.ET_10]: [
-    {
-      title: (
-        <code>
-          <strong>Answer</strong>
-        </code>
-      ),
-      dataIndex: 'answer',
-      render: answer => <code>{answer}</code>
-    },
-    {
-      width: '110px',
-      render: ({ type, id }) => (
-        <Route
-          render={({ history }) => (
-            <div style={{ textAlign: 'center' }}>
-              <Mutation
-                mutation={DELETE_EXERCISE}
-                variables={{ id }}
-                update={(cache, { data: { deleteExercise } }) => {
-                  const { exercises } = cache.readQuery({
-                    query: GET_EXERCISES_BY_TYPE,
-                    variables: { typeUid: type.uid }
-                  });
-
-                  cache.writeQuery({
-                    query: GET_EXERCISES_BY_TYPE,
-                    variables: { typeUid: type.uid },
-                    data: {
-                      exercises: exercises.filter(
-                        ({ id }) => id !== deleteExercise.id
-                      )
-                    }
-                  });
-                }}
-              >
-                {(deleteExercise, { loading }) => (
-                  <Button
-                    size="small"
-                    type="danger"
-                    icon="delete"
-                    loading={loading}
-                    onClick={() =>
-                      onExerciseDeleteConfirm({ onOk: deleteExercise })
-                    }
-                  />
-                )}
-              </Mutation>
-
-              <Divider type="vertical" />
-
-              <Button
-                size="small"
-                icon="eye"
-                onClick={() => {
-                  history.push(`/exercises/${id}`);
-                }}
-              />
-            </div>
-          )}
-        />
-      )
-    }
-  ],
-  [exerciseTypes.ET_20]: [
-    {
-      title: (
-        <code>
-          <strong>Options</strong>
-        </code>
-      ),
-      dataIndex: 'options',
-      render: options => <pre>{JSON.stringify(options, null, 2)}</pre>
-    },
-    {
-      title: (
-        <code>
-          <strong>Answer</strong>
-        </code>
-      ),
-      dataIndex: 'answer',
-      render: answer => <code>{answer}</code>
-    },
-    {
-      width: '110px',
-      render: ({ type, id }) => (
-        <Route
-          render={({ history }) => (
-            <div style={{ textAlign: 'center' }}>
-              <Mutation
-                mutation={DELETE_EXERCISE}
-                variables={{ id }}
-                update={(cache, { data: { deleteExercise } }) => {
-                  const { exercises } = cache.readQuery({
-                    query: GET_EXERCISES_BY_TYPE,
-                    variables: { typeUid: type.uid }
-                  });
-
-                  cache.writeQuery({
-                    query: GET_EXERCISES_BY_TYPE,
-                    variables: { typeUid: type.uid },
-                    data: {
-                      exercises: exercises.filter(
-                        ({ id }) => id !== deleteExercise.id
-                      )
-                    }
-                  });
-                }}
-              >
-                {(deleteExercise, { loading }) => (
-                  <Button
-                    size="small"
-                    type="danger"
-                    icon="delete"
-                    loading={loading}
-                    onClick={() =>
-                      onExerciseDeleteConfirm({ onOk: deleteExercise })
-                    }
-                  />
-                )}
-              </Mutation>
-
-              <Divider type="vertical" />
-
-              <Button
-                size="small"
-                icon="eye"
-                onClick={() => {
-                  history.push(`/exercises/${id}`);
-                }}
-              />
-            </div>
-          )}
-        />
-      )
-    }
-  ]
+  [exerciseTypes.ET_10]: [answerColumn, actionsColumn],
+  [exerciseTypes.ET_20]: [optionsColumn, answerColumn, actionsColumn]
 };
 
 export default exerciseColumns;
