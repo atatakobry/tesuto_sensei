c2a77d0dbf79a35bd9949a27f5c3ca518a78a8a8
Alexandr Loginov <atatakobry@gmail.com>
2018-07-11 12:58:04 +0300

    minor files moves

--- src/common/index.js
+++ src/common/index.js
@@ -1,10 +1,13 @@
 import Breadcrumbs from './Breadcrumbs';
-import { ExerciseCreateModal, ExerciseEditModal } from './ExerciseModals';
+import {
+  ExerciseCreateModal,
+  ExerciseEditModal,
+  onExerciseDeleteConfirm
+} from './ExerciseModals';
 import Loader from './Loader';
 import NavMenu from './NavMenu';
 import NavRoutes from './NavRoutes';
 import ScrollRestoration from './ScrollRestoration';
-import onExerciseDeleteConfirm from './onExerciseDeleteConfirm';
 
 export {
   Breadcrumbs,
